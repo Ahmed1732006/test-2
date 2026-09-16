46edb95056407278fe57044c90e5e21828242141
I don't know <01112367112a@gmail.com>
2026-09-16 22:15:05 +0300

    رفع 14/14

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'sir-app-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './1.png',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    fetch(req).catch(() => caches.match(req).then(r => r || caches.match('./index.html')))
+  );
+});
